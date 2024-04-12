da172269bfd0a3695196eaa5745359914a64a88c
vn-unitbv <norbert.vitalyos@student.unitbv.ro>
2024-04-12 21:34:17 +0300

    readded beach layout and integrated new score and logic changes

--- Assets/Scripts/BallScript.cs
+++ Assets/Scripts/BallScript.cs
@@ -1,5 +1,8 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Linq;
+using TMPro;
+using Unity.VisualScripting;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
@@ -8,13 +11,22 @@ public class BallScript : MonoBehaviour
     // Start is called before the first frame update
     public GameObject Ball;
     public Rigidbody body;
+    public int player1Points;
+    public int player2Points;
+    public TMP_Text Score1;
+    public TMP_Text Score2;
     void Start()
     {
         body = GetComponent<Rigidbody>();
         if (Random.Range(1.0f, 2.0f) > 1.4f)
             body.AddForce(new(0, 0, 2.5f), ForceMode.Impulse);
         else
-            body.AddForce(new(0,0,-2.5f),ForceMode.Impulse);
+            body.AddForce(new(0, 0, -2.5f), ForceMode.Impulse);
+        player1Points = player2Points = 0;
+        Score1 = GameObject.FindGameObjectWithTag("score 1").GetComponentInChildren<TMP_Text>();
+        Score2 = GameObject.FindGameObjectWithTag("score 2").GetComponentInChildren<TMP_Text>();
+        Score1.text += (" " + 0);
+        Score2.text += (" " + 0);
     }
 
     // Update is called once per frame
@@ -25,10 +37,18 @@ public class BallScript : MonoBehaviour
     private void OnCollisionEnter(Collision collision)
     {
         if (collision.gameObject.tag.Equals("Player 1"))
-            body.AddForce(Random.Range(-0.5f, 0.5f), 4, 0.7f, ForceMode.Impulse);
+            body.AddForce(Random.Range(-0.5f, 0.5f), 4, 0.9f, ForceMode.Impulse);
         else if (collision.gameObject.tag.Equals("Player 2"))
-            body.AddForce(Random.Range(-0.5f, 0.5f), 4,-0.7f, ForceMode.Impulse);
-        else if(collision.gameObject.tag.Equals("Plane"))
+            body.AddForce(Random.Range(-0.5f, 0.5f), 4, -0.9f, ForceMode.Impulse);
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.tag.Equals("side 1"))
+            Score1.text = "Score: " + (++player1Points).ToString();
+        else if (other.gameObject.tag.Equals("side 2"))
+            Score2.text = "Score: " + (++player2Points).ToString();
+        else if (other.gameObject.tag.Equals("Plane") || other.gameObject.tag.Equals("court"))
             SceneManager.LoadSceneAsync(1);
     }
 }
--- Assets/Scripts/PlayerScript.cs
+++ Assets/Scripts/PlayerScript.cs
@@ -12,7 +12,7 @@ public class PlayerScript : MonoBehaviour
     public char left;
     void Start()
     {
-        if(up!='w')
+        if (up != 'w')
         {
             up = (char)38;
             down = (char)40;
@@ -26,28 +26,32 @@ public class PlayerScript : MonoBehaviour
     private void ArrowMovement()
     {
         if (Input.GetKey(KeyCode.UpArrow))
-            Player.transform.Translate(-0.05f, 0,0);
+            Player.transform.Translate(-0.05f, 0, 0);
         if (Input.GetKey(KeyCode.DownArrow))
             Player.transform.Translate(0.05f, 0, 0);
         if (Input.GetKey(KeyCode.LeftArrow))
             Player.transform.Translate(0, 0, -0.05f);
         if (Input.GetKey(KeyCode.RightArrow))
             Player.transform.Translate(0, 0, 0.05f);
+        if (Input.GetKey(KeyCode.Space))
+            Player.transform.Translate(0, 0.2f, 0);
     }
     private void KeysMovement()
     {
         if (Input.GetKey(KeyCode.W))
-            Player.transform.Translate(0.05f, 0,0);
+            Player.transform.Translate(0.05f, 0, 0);
         if (Input.GetKey(KeyCode.S))
             Player.transform.Translate(-0.05f, 0, 0);
         if (Input.GetKey(KeyCode.A))
             Player.transform.Translate(0, 0, 0.05f);
         if (Input.GetKey(KeyCode.D))
             Player.transform.Translate(0, 0, -0.05f);
+        if (Input.GetKey(KeyCode.Z))
+            Player.transform.Translate(0, 0.2f, 0);
     }
     void Update()
     {
-        if(up==38)
+        if (up == 38)
             ArrowMovement();
         else
             KeysMovement();
